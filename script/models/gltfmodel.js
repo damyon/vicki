f656554472d8fd6da22caa208434b953bd73f4d9
Damyon Wiese <damyon.wiese@gmail.com>
2020-11-07 21:39:43 +0800

    Base class for gltf model.

---
+++ script/models/gltfmodel.js
@@ -0,0 +1,113 @@
+
+class GLTFModel extends Drawable {
+
+  constructor(index) {
+    super();
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+
+    this.blend = 1;
+    
+    this.gltfModel = null;
+
+    this.modelPath = 'model/a/BoxTextured.gltf';
+  }
+
+  /**
+   * Apply an offset to the position of all the vertices.
+   *
+   */
+  setPosition(gl, x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  getVertexCount() {
+    const mesh = this.gltfModel.meshes[0];
+      
+    return mesh.elementCount;
+  }
+
+  /**
+   * initBuffers
+   *
+   * Initialize the buffers we'll need.
+   */
+  initBuffers(gl) {
+    gltf.loadModel(gl, this.modelPath).then((model) => {
+      this.gltfModel = model;
+    });
+    
+    return this.gltfModel;
+  }
+
+  /**
+   * draw
+   * Draw the rock.
+   * @param gl
+   * @param camera
+   */
+  draw(gl, camera, shadow) {
+    // Tell WebGL how to pull out the positions from the position
+    // buffer into the vertexPosition attribute
+    if (shadow) {
+      gl.uniform1i(camera.isWater, 0);
+    }
+
+    if (this.gltfModel) {
+      const mesh = this.gltfModel.meshes[0];
+      
+      if (shadow) {
+        camera.translate(gl, this.x, this.y, this.z);
+      }
+
+      // Points.
+      gl.bindBuffer(gl.ARRAY_BUFFER, mesh.positions.buffer);
+     
+      // Indices.
+      const vertexPosition = gl.getAttribLocation(camera.lightShaderProgram, 'aVertexPosition');
+      
+      gl.vertexAttribPointer(vertexPosition, mesh.positions.size, mesh.positions.type, false, 0, 0);
+      gl.enableVertexAttribArray(vertexPosition);
+
+      // Tell WebGL how to pull out the texture coordinates from
+      // the texture coordinate buffer into the textureCoord attribute.
+      {
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+
+        const textureCoord = gl.getAttribLocation(camera.cameraShaderProgram, 'aTextureCoord');
+        gl.bindBuffer(gl.ARRAY_BUFFER, mesh.texCoord.buffer);
+        gl.vertexAttribPointer(
+            textureCoord,
+            mesh.texCoord.size,
+            mesh.texCoord.type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(textureCoord);
+      }
+
+
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indices);
+     
+      // Material
+      const materialId = mesh.material;
+      const material = this.gltfModel.materials[materialId];
+
+      if (shadow) {
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, material.baseColorTexture);
+      }
+      
+      gl.drawElements(gl.TRIANGLES, mesh.elementCount, gl.UNSIGNED_SHORT, 0);
+
+    }
+
+  }
+}
