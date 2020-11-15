5731db6557066d9a8c6436b75d71d2de77ed3d31
Damyon Wiese <damyon.wiese@gmail.com>
2020-11-15 21:27:00 +0800

    Bush.

--- script/models/bush.js
+++ script/models/bush.js
@@ -1,11 +1,11 @@
 class Bush extends GLTFModel {
   constructor() {
     super();
-    this.scale = 4;
+    this.scale = 2;
     this.doSwap = 1;
     this.heightOffset = 0;
-    this.blend = 0;
-    this.modelPath = 'model/bush/scene.gltf';
+    this.blend = 1;
+    this.modelPath = 'model/bush/gltf/bush.gltf';
   }
 
 }
