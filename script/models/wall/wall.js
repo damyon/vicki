1d2ff52c713cc3cc7294648d890ccf207a3c548d
Damyon Wiese <dwiese@doghouse.agency>
2022-04-17 17:07:42 +0800

    Rescale the slab.

---
+++ script/models/wall/wall.js
@@ -0,0 +1,20 @@
+class Wall extends VoxelModel {
+  constructor() {
+    super();
+    //this.rotate = Math.random();
+    this.y = 2;
+    this.z = 0;
+    this.x = 8;
+  }
+
+  /**
+   * initBuffers
+   *
+   * Initialize the buffers we'll need.
+   */
+  initBuffers(gl) {
+    this.texture = this.loadTexture(gl, 'script/models/wall/textures/wall.jpg');
+
+    this.loadVoxels(gl, 'script/models/wall/voxels.json');
+  }
+}
